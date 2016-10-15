f8c141b55c2496d4e863d6f487ac77690d1bbeeb
Lucas Estienne <lucas@estienne.sh>
2016-10-14 22:51:14 -0400

    reverse op

--- src/ip6words.py
+++ src/ip6words.py
@@ -3,14 +3,9 @@ import sys
 from iwords import dill_words
 import ip_handling
 
-def main(argv):
-    ip_to_process = parse_args(argv[1:])
-
+def conv_to_words(words, ip_to_process):
     # print("Processing ip...")
     ip = ip_handling.ipv6_arrayize(ip_to_process)
-
-    # print("Loading words...")
-    words = dill_words(ip_handling.get_ipv6_word_possibilities()+1)
 
     # print("Converting ip...")
     ipwords = ip_handling.ip_to_words_arr(ip, words)
@@ -18,9 +13,33 @@ def main(argv):
 
     print(ipstr)
     return ipstr
+
+def conv_to_ipv6(words, ip_to_process):
+    # print("Processing ip...")
+    ip = ip_handling.ip6words_arrayize(ip_to_process)
+
+    # print("Converting ip...")
+    iphex = ip_handling.words_to_ipv6_arr(ip, words)
+    ipstr = ip_handling.iphex_arr_to_str(iphex)
+
+    print(ipstr)
+    return ipstr
+
+def main(argv):
+    ip_to_process, convert_to = parse_args(argv[1:])
+    words = dill_words(ip_handling.get_ipv6_word_possibilities()+1)
+    
+    if convert_to == "words":
+        return conv_to_words(words, ip_to_process)
+    elif convert_to == "ipv6":
+        return conv_to_ipv6(words, ip_to_process)
+    else:
+        raise Exception("Could not determine the type of IP being queried")
     
 def parse_args(args):
     ip_to_process = args[0]
-    return ip_to_process
+    convert_to = ip_handling.get_conversion_type(ip_to_process)
+
+    return ip_to_process, convert_to
 
 if __name__ == "__main__": main(sys.argv)
--- src/ip_handling.py
+++ src/ip_handling.py
@@ -18,6 +18,11 @@ def ipv6_arrayize(ip_addr):
 
     return ip_arr
 
+def ip6words_arrayize(ip_addr):
+    ip_arr = ip_addr.split(".")
+
+    return ip_arr
+
 def ip_to_words_arr(ip_arr, words_arr):
     if(len(words_arr) < get_ipv6_word_possibilities()):
         raise Exception("Not enough words")
@@ -26,5 +31,47 @@ def ip_to_words_arr(ip_arr, words_arr):
     
     return ipwords
 
+def words_to_ipv6_arr(ip_arr, words_arr):
+    if(len(words_arr) < get_ipv6_word_possibilities()):
+        raise Exception("Not enough words")
+    def get_wkey(word):
+        for i, value in enumerate(words_arr):
+            if word == value:
+                return i
+        raise Exception("Could not find word in words array")
+    
+    ipdec = []
+    iphex = []
+    
+    for ipw in ip_arr:
+        ipdec += [get_wkey(ipw)]
+    for ipd in ipdec:
+        iphex += [hex(ipd).split('x')[1]]
+
+    #ipwords = [words_arr[iw] for iw in ip_arr]
+    
+    return iphex
+
 def words_arr_to_str(words_arr):
     return '.'.join(words_arr)
+
+def iphex_arr_to_str(iphex_arr):
+    iphex = ':'.join(iphex_arr)
+    iphex = str(ipaddress.IPv6Address(iphex).compressed)
+    return iphex
+
+def get_conversion_type(ip):
+    if len(ip.split(":")) > 1 or ip[:2] == "::":
+        if len(ip.split(":")) < 2 and ip[:2] == "::":
+            return "words"
+        elif len(ip.split(":")) != 8 and len(ip.split(":")) > 0 and "::" in ip:
+            return "words"
+        elif len(ip.split(":")) == 8:
+            return "words"
+    
+    elif len(ip.split(".")) == 4:
+        raise Exception("Cannot convert IPv4 addresses!")
+    elif len(ip.split(".")) == 8:
+        return "ipv6"
+
+    return None
